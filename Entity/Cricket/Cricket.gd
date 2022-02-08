76fe5f60dc92004a4f0cc27ef914791b3b947d1a
xnguyena2 <xnguyena2@yahoo.com>
2022-02-08 22:04:53 +0700

    [add melee]

--- Entity/Cricket/Cricket.gd
+++ Entity/Cricket/Cricket.gd
@@ -8,22 +8,41 @@ var is_flip = false
 
 onready var _critket = $Critket
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+onready var _ani_tree = $AnimationTree
+onready var _ani_playback = $AnimationTree.get("parameters/playback")
+onready var _idle_blend_pos = $AnimationTree.get("parameters/Idle/blend_position")
+onready var _run_blend_pos = $AnimationTree.get("parameters/Idle/blend_position")
+onready var _melee_blend_pos = $AnimationTree.get("parameters/Melee/blend_position")
 
 
 func _physics_process(delta):
+	var is_attacking = false
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
+	if Input.is_action_just_pressed("melee"):
+		direction = Vector2.ZERO
+		is_attacking = true
+	if direction == Vector2.ZERO:
+		if !is_attacking:
+			_ani_playback.travel("Idle")
+		else:
+			print("attack")
+			_ani_playback.travel("Melee")
+	else:
+		_idle_blend_pos = direction
+		_run_blend_pos = direction
+		_melee_blend_pos = direction
+		_ani_playback.travel("Running")
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	var snap: Vector2 = transform.y * 10 if direction.y == 0.0 else Vector2.ZERO
-	if is_on_floor():
-		rotation = get_floor_normal().angle() + PI/2
+	#if is_on_floor():
+	#	rotation = get_floor_normal().angle() + PI/2
+	#_velocity.x *= delta*80
 	_velocity = move_and_slide_with_snap(
-		_velocity.rotated(rotation), snap, -transform.y, true
+		_velocity#.rotated(rotation)
+		, snap, -transform.y, true
 	)
-	_velocity = _velocity.rotated(-rotation)
+	#_velocity = _velocity.rotated(-rotation)
 
 func _input(event):
 	var direction: = get_direction()
